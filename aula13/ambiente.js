4aa3b13c670b6ec040a5cc7a92d1861694a718f6
Chidarengan <lucasdesamartins@hotmail.com>
2025-03-17 22:15:56 -0400

    adicionado aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    console.log(`passo ${c}`)
+    c++
+}
+
+var c = 1
+do {
+    console.log('Tudo bem?')
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 10)
